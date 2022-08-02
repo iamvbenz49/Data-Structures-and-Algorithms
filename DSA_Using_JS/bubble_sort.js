434c060b9a34b8d85e3f5914f53aeee8ab3f1a73
Vijay Benz <benzwrites@gmail.com>
2022-08-02 17:19:38 +0530

    Create bubble_sort.js

---
+++ DSA_Using_JS/bubble_sort.js
@@ -0,0 +1,21 @@
+class bubble_sort {
+    constructor(arr) {
+        this.arr = arr
+    }
+    algo() {
+        let i, j;
+        for (i = 0; i < this.arr.length - 1; i++) {
+            for (j = 0; j < this.arr.length - i; j++) {
+                if (this.arr[j] > this.arr[j + 1]) {
+                    let temp = this.arr[j]
+                    this.arr[j] = this.arr[j + 1]
+                    this.arr[j + 1] = temp
+                }
+            }
+        }
+        return this.arr
+    }
+}
+
+const algo = new bubble_sort([9, 7, 3, 6, 2])
+console.log(algo.algo())
