acad88832511dc73bc984987f37574428fafb74d
Sham Vijay <benzwrites@gmail.com>
2022-12-15 22:44:24 +0530

    Update mngrid.py

--- dynamicProgramming/mngrid.py
+++ dynamicProgramming/mngrid.py
@@ -11,3 +11,7 @@ def grid(m,n):
     memo[m,n]=memo[n,m] = grid(m-1,n) + grid(m,n-1)
     return memo[m,n]
 print(grid(18,121))
+
+"""
+   you can only take right and front to reach end in (m,n)
+ grid"""
